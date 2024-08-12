e12a3e7a043be257ebdcca16a1e15fbc458e2b6a
saviodc <savio.concessio@outlook.com>
2024-08-12 16:01:05 +1200

    Monster with enum and State interface route

--- src/main/Monster.java
+++ src/main/Monster.java
@@ -4,14 +4,60 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import imgs.Img;
 
-abstract class Monster implements Entity {
-	static class MonsterException extends Error{
-		private static final long serialVersionUID = 1L;
-		public MonsterException(String message) {
-	        super(message);
-	    }
-	}
+class MonsterException extends Error{
+	private static final long serialVersionUID = 1L;
+	public MonsterException(String s) {
+        super("Dead Monster");
+    }
+}
+interface MonsterState{
+	 Point location();
+	 //default double speed() {}
+	 default void location(Point p, Monster m) {}
+	 void ping(Model m, Monster mon);
+	 double chaseTarget(Monster outer, Point target, Monster mon);
+	 void draw(Graphics g, Point center, Dimension size, Monster mon);
+}
+enum MonsterStates implements MonsterState{
+	Awake(){
+		public void ping(Model m, Monster mon) {
+			var arrow = m.camera().location().distance(mon.location());
+			double size = arrow.size();
+			arrow = arrow.times(mon.speed() / size);
+			mon.location(mon.location().add(arrow));
+			if (size < 0.6d) {
+				m.onGameOver();
+			}
+		}
+		public double chaseTarget(Monster outer, Point target, Monster mon) {
+			var arrow = target.distance(outer.location());
+			double size = arrow.size();
+			arrow = arrow.times(mon.speed() / size);
+			outer.location(outer.location().add(arrow));
+			return size;
+		}
+		public void draw(Graphics g, Point center, Dimension size, Monster mon) {
+			mon.drawImg(Img.AwakeMonster.image, g, center, size);
+		}
+	},
+	Dead,
+	Sleep;
+}
+//record AwakeMonster() implements MonsterState{}
+//record DeadMonster() implements MonsterState{}
+//record SleepMonster() implements MonsterState{}
+
+
+class Monster implements Entity {
+	 
+	MonsterStates state = MonsterState.Sleep;
 	private Point location;
+	Monster(Point location) {
+		this.location = location;
+	}
+
+	public double speed() {
+	
 
 	public Point location() {
 		return location;
@@ -21,11 +67,6 @@ abstract class Monster implements Entity {
 		location = p;
 	}
 
-	Monster(Point location) {
-		this.location = location;
-	}
-
-	public double speed() {
 		return 0.05d;
 	}
 
@@ -51,58 +92,8 @@ abstract class Monster implements Entity {
 		drawImg(Img.AwakeMonster.image, g, center, size);
 	}
 
-	class AliveMonster extends Monster {
 
-		AliveMonster(Point location) {
-			super(location);
-		}
-		@Override
-		public void draw(Graphics g, Point center, Dimension size) {
-			drawImg(Img.AwakeMonster.image, g, center, size);
-		}
-	}
-	
-	class SleepMonster extends Monster {
 
-		SleepMonster(Point location) {
-			super(location);
-		}
-		@Override
-		public void draw(Graphics g, Point center, Dimension size) {
-			drawImg(Img.SleepMonster.image, g, center, size);
-		}
-	}
 
-	class DeadMonster extends Monster {
-		
-		DeadMonster(Point location) {
-			super(location);
-		}
-		
-		@Override
-		public Point location() {
-			throw new MonsterException("Dead Monster");
-		}
-
-		public void location(Point p) {
-			throw new MonsterException("Dead Monster");
-		}
-
-		public double speed() {
-			throw new MonsterException("Dead Monster");		
-		}
-
-		public void ping(Model m) {
-			throw new MonsterException("Dead Monster");
-		}
-
-		public double chaseTarget(Monster outer, Point target) {
-			throw new MonsterException("Dead Monster");
-		}
-
-		public void draw(Graphics g, Point center, Dimension size) {
-			drawImg(Img.DeadMonster.image, g, center, size);
-		}
-	}
 
 }
